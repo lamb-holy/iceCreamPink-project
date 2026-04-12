e580c23c07983b25850d3f9596419d243530dd8f
skywhywalk3r <mgleb830@gmail.com>
2026-04-12 13:43:07 +0300

    nothing

---
+++ sass/modal.js
@@ -0,0 +1,18 @@
+(() => {
+  const refs = {
+    openModalBtns: document.querySelectorAll("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtns.forEach(btn => {
+    btn.addEventListener("click", toggleModal);
+  });
+
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
+  }
+})();
