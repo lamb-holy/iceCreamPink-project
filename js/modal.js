32e15df7d54d19349ba43e324888fada3a8fab96
lamb-holy <daryna.shymchuk@gmail.com>
2026-04-23 23:22:08 +0300

    IM SO HAPPY IM JUMPING AROUND RAHHHHH
    header, 5-s modals are done

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,23 @@
+// header modal
+
+;(() => {
+	const refs = {
+		openModalBtn: document.querySelector('[data-modal-open-header]'),
+		closeModalBtn: document.querySelector('[data-modal-close-header]'),
+		modal: document.querySelector('[data-modal-header]'),
+	}
+
+	refs.openModalBtn.addEventListener('click', toggleModal)
+	refs.closeModalBtn.addEventListener('click', toggleModal)
+
+	function toggleModal() {
+		refs.modal.classList.toggle('is-hidden-header')
+		document.body.classList.toggle('no-scroll')
+	}
+})()
+
+// products modal
+
 ;(() => {
 	const refs = {
 		openModalBtn: document.querySelector('[data-modal-open-product]'),
@@ -10,6 +30,24 @@
 
 	function toggleModal() {
 		refs.modal.classList.toggle('is-hidden-product')
+		document.body.classList.toggle('no-scroll')
+	}
+})()
+
+// location modal
+
+;(() => {
+	const refs = {
+		openModalBtn: document.querySelector('[data-modal-open-loc]'),
+		closeModalBtn: document.querySelector('[data-modal-close-loc]'),
+		modal: document.querySelector('[data-modal-loc]'),
+	}
+
+	refs.openModalBtn.addEventListener('click', toggleModal)
+	refs.closeModalBtn.addEventListener('click', toggleModal)
+
+	function toggleModal() {
+		refs.modal.classList.toggle('is-hidden-loc')
 		document.body.classList.toggle('no-scroll')
 	}
 })()
