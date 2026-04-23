8c4dd1a9d062e550f8ae4137401a0daf67a12c88
Gleb <mgleb830@gmail.com>
2026-04-23 21:41:19 +0300

    MODALS DONE

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,4 @@
-;(() => {
+;;(() => {
 	const refs = {
 		openModalBtn: document.querySelector('[data-modal-open-product]'),
 		closeModalBtn: document.querySelector('[data-modal-close-product]'),
@@ -13,3 +13,36 @@
 		document.body.classList.toggle('no-scroll')
 	}
 })()
+;;(() => {
+	const refs = {
+		openModalBtn: document.querySelector('[data-modal-open-product2]'),
+		closeModalBtn: document.querySelector('[data-modal-close-product2]'),
+		modal: document.querySelector('[data-modal-product2]'),
+	}
+
+	refs.openModalBtn.addEventListener('click', toggleModal)
+	refs.closeModalBtn.addEventListener('click', toggleModal)
+
+	function toggleModal() {
+		refs.modal.classList.toggle('is-hidden-product2')
+		document.body.classList.toggle('no-scroll')
+	}
+})()
+;(() => {
+	const refs = {
+		openModalBtn: document.querySelector('[data-modal-open-product3]'),
+		closeModalBtn: document.querySelector('[data-modal-close-product3]'),
+		modal: document.querySelector('[data-modal-product3]'),
+	}
+
+	refs.openModalBtn.addEventListener('click', toggleModal)
+	refs.closeModalBtn.addEventListener('click', toggleModal)
+
+	function toggleModal() {
+		refs.modal.classList.toggle('is-hidden-product3')
+		document.body.classList.toggle('no-scroll')
+	}
+})()
+
+
+
